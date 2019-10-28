386cb317c7db052278d3a29013ca0726032919a2
matthew ho <matthewho@umail.ucsb.edu>
2019-10-28 00:38:08 -0700

    init

---
+++ precache-manifest.5423159bf77c79ba539812422550190d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30841c5581a39e94599346a7e8b075d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d686d65b8d20f094785",
+    "url": "/static/css/main.58604cc9.chunk.css"
+  },
+  {
+    "revision": "94ef3dec08755e6b1e9e",
+    "url": "/static/js/2.550a15d2.chunk.js"
+  },
+  {
+    "revision": "6d686d65b8d20f094785",
+    "url": "/static/js/main.b98792a7.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
