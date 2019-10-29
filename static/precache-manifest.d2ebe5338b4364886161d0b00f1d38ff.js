c1c068514abd978245ee75c3eec876a57c4fe25a
matthew ho <matthewho@umail.ucsb.edu>
2019-10-28 21:19:52 -0700

    new build

---
+++ static/precache-manifest.d2ebe5338b4364886161d0b00f1d38ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a55b399477c2ba6a336e5ab4a7745d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32b51880646b7ba694b6",
+    "url": "./static/css/main.58604cc9.chunk.css"
+  },
+  {
+    "revision": "94ef3dec08755e6b1e9e",
+    "url": "./static/js/2.550a15d2.chunk.js"
+  },
+  {
+    "revision": "32b51880646b7ba694b6",
+    "url": "./static/js/main.fd012617.chunk.js"
+  },
+  {
+    "revision": "8020b650e5f5db8a485c",
+    "url": "./static/js/runtime-main.62e41b83.js"
+  }
+]);
---
+++ static/precache-manifest.dc0422a343a295ca9bff9b43c1e44256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab65ec77b0f595bf12a323e7ccd245e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74cd6f030a467f24c4c5",
+    "url": "./static/css/main.58604cc9.chunk.css"
+  },
+  {
+    "revision": "94ef3dec08755e6b1e9e",
+    "url": "./static/js/2.550a15d2.chunk.js"
+  },
+  {
+    "revision": "74cd6f030a467f24c4c5",
+    "url": "./static/js/main.50aab071.chunk.js"
+  },
+  {
+    "revision": "8020b650e5f5db8a485c",
+    "url": "./static/js/runtime-main.62e41b83.js"
+  }
+]);
